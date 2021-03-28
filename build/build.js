e59d78b7d84a9d7ea5f80ef28394b4eac0fcdbcb
Margot <togramf2@gmail.com>
2021-03-28 17:13:50 +0200

    Yeah

--- build/build.js
+++ build/build.js
@@ -1,54 +1,139 @@
 var gui = new dat.GUI();
 var params = {
-    y: 2,
-    x: 5,
-    h: 0.5,
-    w: 0.5,
-    n: 0.5,
-    m: 1,
-    scale: 400,
+    background: 0,
+    origin: 0,
+    bezier: 1,
+    curve: 0,
+    trigo1: 0,
+    trigo2: 0,
+    trigo3: 0,
+    y: 0,
+    x: 0,
+    scale: 40,
+    h: 0.25,
+    w: 0.9,
+    n: 0,
+    m: 0,
+    trigox: 2,
+    trigoy: 5,
     Download_Image: function () { return save(); },
 };
-gui.add(params, "y", 0, 20, 0.01);
-gui.add(params, "x", 0, 20, 0.1);
+gui.add(params, "background", 0, 1, 1);
+gui.add(params, "origin", 0, 1, 1);
+gui.add(params, "bezier", 0, 1, 1);
+gui.add(params, "curve", 0, 1, 1);
+gui.add(params, "trigo1", 0, 1, 1);
+gui.add(params, "trigo2", 0, 1, 1);
+gui.add(params, "trigo3", 0, 1, 1);
+gui.add(params, "y", -5, 5, 0.001);
+gui.add(params, "x", -5, 5, 0.001);
+gui.add(params, "scale", 0, 500, 0.1);
 gui.add(params, "h", 0, 1, 0.01);
 gui.add(params, "w", 0, 1, 0.01);
-gui.add(params, "n", 0, 20, 0.01);
-gui.add(params, "m", 1, 20, 0.1);
-gui.add(params, "scale", 0, 500, 0.1);
+gui.add(params, "n", -5, 5, 0.01);
+gui.add(params, "m", -5, 5, 0.01);
+gui.add(params, "trigox", -50, 50, 0.1);
+gui.add(params, "trigoy", -50, 50, 0.1);
 gui.add(params, "Download_Image");
 function draw() {
-    background(240);
-    translate(width / 2, height / 2);
-    strokeWeight(3);
-    stroke(0);
+    if (params.background)
+        background(255);
+    translate(width / 2 + params.x * params.scale, height / 2 + params.y * params.scale);
+    strokeWeight(2);
     noFill();
-    motif(1, -1);
-    stroke(255, 102, 0);
-    motif(-1, -1);
-    stroke(102, 255, 0);
-    motif(1, 1);
-    stroke(0);
-    motif(-1, 1);
+    if (params.origin) {
+        drawOrigin();
+        stroke(0);
+    }
+    if (params.bezier) {
+        motifBezier(1, -1);
+        motifBezier(-1, -1);
+        motifBezier(1, 1);
+        motifBezier(-1, 1);
+    }
+    else if (params.curve) {
+        motifCurve(1, -1);
+        motifCurve(-1, -1);
+        motifCurve(1, 1);
+        motifCurve(-1, 1);
+    }
+    else if (params.trigo1) {
+        trigo1();
+    }
+    else if (params.trigo2) {
+        trigo2(1);
+        trigo2(-1);
+    }
+    else if (params.trigo3) {
+        trigo3();
+    }
 }
-function motif(a, b) {
+function drawOrigin() {
+    stroke(255, 0, 0);
+    line(-width / 2, 0, width / 2, 0);
+    line(0, -height / 2, 0, height / 2);
+}
+function motifBezier(a, b) {
     var w = params.w;
     var h = params.h;
     push();
-    for (var i = 0.2; i < 1.2; i += 0.2) {
-        beginShape();
-        curveVertex(a * 0 * w * params.scale, b * 3 * h * params.scale);
-        curveVertex(a * 1 * w * params.scale, b * 1.5 * h * params.scale);
-        curveVertex(a * 0 * w * params.scale, b * .5 * h * params.scale);
-        curveVertex(a * 0.1 * w * params.scale, b * .1 * h * params.scale);
-        curveVertex(a * 3 * w * params.scale, b * 0 * h * params.scale);
-        curveVertex(a * 6 * w * params.scale, b * 1.5 * h * params.scale);
-        endShape(CLOSE);
-        translate(0, b * (3 * h / 2 * params.scale));
-        w = w / exp(i);
-        h = h * exp(i);
+    translate(a * params.m * params.scale, b * params.n * params.scale);
+    for (var i = 0; i < 1.5; i += 0.3) {
+        w = w / 2;
+        h = h * exp(i * 0.4);
+        bezier(a * 0 * w * params.scale, b * 6 * h * params.scale, a * 2 * w * params.scale, b * 3 * h * params.scale, a * (-1.5 + i) / 3 * w * params.scale, b * 1.5 * h * params.scale, a * 0.1 * w * params.scale, b * 0.35 * h * params.scale);
+        bezier(a * 0.1 * w * params.scale, b * .35 * h * params.scale, a * .2 * w * params.scale, b * 0 * h * params.scale, a * 2 * w * params.scale, b * -0.1 * h * params.scale, a * 4 * w * params.scale, b * 0.3 * h * params.scale);
+        bezier(a * 4 * w * params.scale, b * 0.3 * h * params.scale, a * 5 * w * params.scale, b * .5 * h * params.scale, a * 12 * w * params.scale, b * 2 * h * params.scale, a * 0 * w * params.scale, b * 6 * h * params.scale);
+        translate(0, b * (6 * h / 3) * params.scale);
     }
     pop();
+}
+function motifCurve(a, b) {
+    var w = params.w;
+    var h = params.h;
+    push();
+    translate(a * params.m * params.scale, b * params.n * params.scale);
+    for (var i = 0; i < 1.5; i += 0.3) {
+        w = w / 2;
+        h = h * exp(i * 0.4);
+        beginShape();
+        curveVertex(a * 0 * w * params.scale, b * 6 * h * params.scale);
+        curveVertex(a * 1.5 * w * params.scale, b * 3 * h * params.scale);
+        curveVertex(a * 0 * w * params.scale, b * 1 * h * params.scale);
+        curveVertex(a * 1 * w * params.scale, b * 0 * h * params.scale);
+        curveVertex(a * 4 * w * params.scale, b * .2 * h * params.scale);
+        curveVertex(a * 6.5 * w * params.scale, b * 2 * h * params.scale);
+        endShape(CLOSE);
+        translate(0, b * (6 * h / 3) * params.scale);
+    }
+    pop();
+}
+function trigo1() {
+    beginShape();
+    for (var a = 0; a < TWO_PI; a += 0.001) {
+        var x = cos(a * params.trigox);
+        var y = a * params.m - params.n * sin(a * params.trigoy);
+        vertex(x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x)) * params.h * params.scale);
+    }
+    endShape();
+}
+function trigo2(i) {
+    beginShape();
+    for (var a = 0; a < TWO_PI; a += 0.0001) {
+        var x = -params.m * cos(a * params.trigox);
+        var y = i * params.n * sin(a * params.trigoy) * sin(a * params.trigoy);
+        vertex(x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x)) * params.h * params.scale);
+    }
+    endShape();
+}
+function trigo3() {
+    beginShape();
+    for (var a = 0; a < TWO_PI; a += 0.001) {
+        var x = cos(a * params.trigox);
+        var y = params.n * sin(a * params.trigoy) * tan(params.m * a);
+        vertex(x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x)) * params.h * params.scale);
+    }
+    endShape();
 }
 function setup() {
     p6_CreateCanvas();
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,5 +1,6 @@
-//RECODING V1
-//08/03/2021
+//RECODING 
+//ALGEBRAISCHE KURVEN
+//MARGOT FAUCHON 
 
 // -------------------
 //  Parameters and UI
@@ -7,22 +8,42 @@
 
 const gui = new dat.GUI()
 const params = {
-    y: 2,
-    x: 5,
-    h: 0.5,
-    w: 0.5,
-    n: 0.5,
-    m: 1,
-    scale : 400,
+    background: 0,
+    origin: 0,
+    bezier: 1,
+    curve: 0,
+    trigo1: 0,
+    trigo2: 0,
+    trigo3: 0,
+    y: 0,
+    x: 0,
+    scale :40,
+    h: 0.25,
+    w: 0.9,
+    n: 0,
+    m: 0,
+    trigox: 2,
+    trigoy: 5,
+    
     Download_Image: () => save(),
 }
-gui.add(params, "y", 0, 20, 0.01)
-gui.add(params, "x", 0, 20, 0.1)
+
+gui.add(params, "background", 0, 1,1)
+gui.add(params, "origin", 0, 1,1)
+gui.add(params, "bezier", 0, 1,1)
+gui.add(params, "curve", 0, 1,1)
+gui.add(params, "trigo1", 0, 1,1)
+gui.add(params, "trigo2", 0, 1,1)
+gui.add(params, "trigo3", 0, 1,1)
+gui.add(params, "y", -5, 5, 0.001)
+gui.add(params, "x", -5, 5, 0.001)
+gui.add(params, "scale", 0, 500, 0.1)
 gui.add(params, "h", 0, 1, 0.01)
 gui.add(params, "w", 0, 1, 0.01)
-gui.add(params, "n", 0, 20, 0.01)
-gui.add(params, "m", 1, 20, 0.1)
-gui.add(params, "scale", 0, 500, 0.1)
+gui.add(params, "n", -5, 5, 0.01)
+gui.add(params, "m", -5, 5, 0.01)
+gui.add(params, "trigox", -50,50,0.1)
+gui.add(params, "trigoy", -50,50,0.1)
 gui.add(params, "Download_Image")
 
 // -------------------
@@ -30,93 +51,126 @@ gui.add(params, "Download_Image")
 // -------------------
 
 function draw() {
-    background(240)
-    translate(width/2, height/2)
-    strokeWeight(3)
-    stroke(0)
+    if (params.background)
+        background(255)
+
+    translate(width/2 + params.x * params.scale, height/2 + params.y * params.scale)
+    strokeWeight(2)
     noFill()
 
-    /*beginShape()
-    let w = 1;
-    let h = 0.5;
-    for (let a = 0; a < TWO_PI; a += 0.01) {
-        const y = sin(w* a)
-        const x = cos(h* a) * tan(params.y)
-        vertex(x * height * 0.3, y * height * 0.3)
+    if (params.origin){
+        drawOrigin()
+        stroke(0)
     }
-    endShape(CLOSE)*/
-
-    /*beginShape()
-    for (let a = 0; a < TWO_PI; a+= 0.0001){
-        const x = cos(params.n*a) 
-        const y = sin(params.m*a)
-        vertex (x * height *0.3, y* height * 0.3 * exp(-abs(x)))
         
+
+    if (params.bezier){
+        motifBezier(1,-1)
+        motifBezier(-1,-1)
+        motifBezier(1,1)
+        motifBezier(-1,1)
+
+    } else if (params.curve){
+        motifCurve(1,-1)
+        motifCurve(-1,-1)
+        motifCurve(1,1)
+        motifCurve(-1,1)
+
+    } else if (params.trigo1){
+        trigo1()
+
+    } else if (params.trigo2){
+        trigo2(1)
+        trigo2(-1)
+
+    } else if (params.trigo3){
+        trigo3()
     }
-    endShape(CLOSE)*/
 
-    //Version ok 27.03
-    // beginShape()
-    // for (let a=0; a<TWO_PI; a+= 0.001){
-    //     const x = cos (a * params.x) 
-    //     const y = a * params.y - params.n * sin(a * params.m)
-    //     vertex (x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x))* params.h * params.scale)
-    // }
-    // endShape()
-
-    // beginShape()
-    // for (let a = 0; a<PI/4; a+= 0.001){
-    //     const x = cos(a)
-    //     const y = sin (a - PI/6) * sin (a - PI/6) 
-    //     vertex (x * height * 0.3, y * height * 0.3)
-    // }
-    // endShape()
-
-    //essai ok 2
-    // beginShape()
-    // for (let a=0; a<TWO_PI; a+= 0.0001){
-    //     const x =  - cos (a * params.x) 
-    //     const y =  - params.n * sin(a * params.m) * sin(a * params.m)
-    //     vertex (x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x))* params.h * params.scale)
-    // }
-    // endShape()
-
-    // beginShape()
-    // for (let a=0; a<TWO_PI; a+= 0.01){
-    //     const y = sin(a * params.y)
-    //     const x = cos (a * params.x)  * tan(y * params.n)
-    //     vertex (x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x)) * params.h * params.scale)
-    // }
-    // endShape()
-
-    motif(1,-1);
-    stroke(255, 102, 0)
-    motif(-1,-1);
-    stroke(102, 255,0)
-    motif(1,1);
-    stroke(0)
-    motif(-1,1);
-        
 }   
 
-function motif( a,  b){
+function drawOrigin(){
+    stroke(255, 0 ,0)
+    line(-width/2, 0 , width/2, 0)
+    line(0 ,-height/2, 0 , height/2)
+}
+
+function motifBezier(a, b){
+    let w = params.w;
+    let h = params.h; 
+
+    push()
+    translate(a* params.m * params.scale, b* params.n * params.scale)
+    for (let i = 0; i <1.5 ; i+=0.3){
+        w=w/2;
+        h=h *exp(i*0.4);
+        bezier( a* 0 * w * params.scale, b* 6 * h  * params.scale,
+            a* 2 * w  * params.scale, b*3* h  * params.scale, 
+            a* (-1.5 + i)/3 * w  * params.scale, b*1.5* h  * params.scale, 
+            a* 0.1 * w  * params.scale, b*0.35* h  * params.scale)
+        bezier ( a* 0.1 * w  * params.scale, b* .35* h  * params.scale,
+            a * .2 * w * params.scale, b * 0 * h * params.scale,
+            a* 2 * w  * params.scale, b* -0.1* h  * params.scale,
+            a* 4 * w  * params.scale, b*0.3* h  * params.scale)
+        bezier( a* 4 * w  * params.scale, b*0.3* h  * params.scale,
+            a* 5 * w  * params.scale, b*.5* h  * params.scale,
+            a* 12 * w  * params.scale, b*2* h  * params.scale,
+            a* 0 * w * params.scale, b* 6 * h  * params.scale)
+        translate(0, b*(6 * h /3) * params.scale);
+    }
+    pop()
+}
+
+function motifCurve(a,b){
     let w = params.w;
     let h = params.h; 
     push()
-    for (let i = 0.2; i <1.2 ; i+=0.2){
+    translate(a* params.m * params.scale, b* params.n * params.scale)
+    for (let i = 0; i <1.5 ; i+=0.3){
+        w=w/2;
+        h=h *exp(i*0.4);
         beginShape()
-            curveVertex(a* 0 * w * params.scale,b* 3 * h  * params.scale)
-            curveVertex(a* 1 * w  * params.scale,b*1.5* h  * params.scale)
-            curveVertex(a* 0 * w  * params.scale,b*.5* h  * params.scale)
-            curveVertex(a* 0.1 * w  * params.scale,b*.1* h  * params.scale)
-            curveVertex(a* 3 * w  * params.scale,b*0* h  * params.scale)
-            curveVertex(a* 6 * w  * params.scale,b*1.5* h  * params.scale)
+            curveVertex( a* 0 * w * params.scale, b* 6 * h  * params.scale)
+            curveVertex(a* 1.5 * w  * params.scale, b*3* h  * params.scale) 
+            curveVertex( a* 0 * w  * params.scale, b*1* h  * params.scale)
+            curveVertex(a * 1 * w * params.scale, b * 0 * h * params.scale)
+            curveVertex(a* 4 * w  * params.scale, b*.2* h  * params.scale)
+            curveVertex(a* 6.5 * w  * params.scale, b*2* h  * params.scale)
+            
         endShape(CLOSE)
-        translate(0, b*(3 * h/2 * params.scale));
-        w=w/exp(i);
-        h=h *exp(i); 
+        translate(0, b*(6 * h /3) * params.scale);
     }
     pop()
+}
+
+function trigo1(){
+    beginShape()
+        for (let a=0; a<TWO_PI; a+= 0.001){
+            const x = cos (a * params.trigox) 
+            const y = a * params.m - params.n * sin(a * params.trigoy)
+            vertex (x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x))* params.h * params.scale)
+        }
+    endShape()
+}
+
+function trigo2 (i){
+    beginShape()
+    for (let a=0; a<TWO_PI; a+= 0.0001){
+        const x =  - params.m * cos (a * params.trigox) 
+        const y =  i* params.n * sin(a * params.trigoy) * sin(a * params.trigoy)
+        vertex (x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x))* params.h * params.scale)
+    }
+    endShape()
+}
+
+function trigo3(){
+    beginShape()
+        for (let a=0; a<TWO_PI; a+= 0.001){
+            const x = cos (a * params.trigox) 
+            const y = params.n * sin(a * params.trigoy) * tan(params.m * a)
+            vertex (x * exp(-abs(y)) * params.w * params.scale, y * exp(-abs(x))* params.h * params.scale)
+        }
+    endShape()
 }
 
 // -------------------
